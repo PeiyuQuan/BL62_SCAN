5376650614a1fe425626192454bb09f17f8c4846
Quan, Peiyu <41603034+PeiyuQuan@users.noreply.github.com>
2024-03-30 18:07:35 -0700

    Update BL62_Move_Energy.py

--- Energy_scan_code/BL62_Move_Energy.py
+++ Energy_scan_code/BL62_Move_Energy.py
@@ -12,9 +12,9 @@ def energy(mono):
 	if fmono >= 3000.0 and fmono <= 13000.0:
 		m1 = 2*dspacing*fmono
 		crystal = math.asin(hc/m1)
-		angle_crystal=int((180.0*crystal/math.pi) * 1000)/1000.0
+		angle_crystal=float(180.0*crystal/math.pi)
 		print(str(fmono) + " eV" +" : " + str(angle_crystal))
-		crystal_1.put(angle_crystal.value, wait=True)
+		crystal_1.put(angle_crystal, wait=True)
 		print(str(crystal_1.value))
 	else:
 		print("Energy is out of range")
