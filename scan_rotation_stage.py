a7c0666043a355480c0ceafe5be3ae3af957c4ff
Quan, Peiyu <41603034+PeiyuQuan@users.noreply.github.com>
2024-05-11 14:54:59 -0700

    Update scan_rotation_stage.py

--- scan_rotation_stage.py
+++ scan_rotation_stage.py
@@ -4,6 +4,13 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
+def set_zero():
+    epics.PV("BL62:DMC01:A.SET").put('Set',wait=True)
+    time.sleep(0.5)
+    epics.PV("BL62:DMC01:A.VAL").put(0, wait=True)
+    epics.PV("BL62:DMC01:A.SET").put('Use',wait=True)
+    time.sleep(0.5)
+    
 def scan_rotation_stage(start, step, points, mono, off_sample,file_name,Image_Num,bg_exp_time,exp_time):
     epics.PV("BL62:DMC01:A.SET").put('Set',wait=True)
     time.sleep(0.5)
