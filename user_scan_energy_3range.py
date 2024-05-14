b7818903d0b3b88f54fba2d9349753124603b737
Quan, Peiyu <41603034+PeiyuQuan@users.noreply.github.com>
2024-05-13 17:57:24 -0700

    Add files via upload

---
+++ user_scan_energy_3range.py
@@ -0,0 +1,3 @@
+from scan_energy_range import scan_energy_3_ranges 
+scan_energy_3_ranges(8000, 5, 5, 8051, 1, 5, 8110, 10, 5, off_sample=8, file_name="test.txt", exposure_time=0.05)
+        
---
+++ user_scan_energy_4range.py
@@ -0,0 +1,3 @@
+from scan_energy_range import scan_energy_4_ranges
+scan_energy_4_ranges(8000, 5, 6, 8051, 1, 6, 8110, 10, 6, 8280, 10, 6, off_sample=8,file_name="test.txt", exposure_time=0.05)
+        
