2ddc85b063b9d8532d9c7a9f72fd3b3ab87308d7
Quan, Peiyu <41603034+PeiyuQuan@users.noreply.github.com>
2025-02-06 23:52:02 -0800

    Update filter.py

--- Energy_scan_code/filter.py
+++ Energy_scan_code/filter.py
@@ -5,79 +5,79 @@ def filter_number(n):
     epics.PV("BL62:E1608_1:Bo2").put("Low", wait=True)
     epics.PV("BL62:E1608_1:Bo3").put("Low", wait=True)
     epics.PV("BL62:E1608_1:Bd4").put("In", wait=True)
-    
-    if n<16:
-        if n==0:
+
+    if n < 16:
+        if n == 0:
             epics.PV("BL62:E1608_1:Bd1").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("In", wait=True)
-        elif n==1:
+        elif n == 1:
             epics.PV("BL62:E1608_1:Bd1").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("In", wait=True)
-        elif n==2:
+        elif n == 2:
             epics.PV("BL62:E1608_1:Bd1").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("In", wait=True)
-        elif n==3:
+        elif n == 3:
             epics.PV("BL62:E1608_1:Bd1").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("In", wait=True)
-        elif n==4:
+        elif n == 4:
             epics.PV("BL62:E1608_1:Bd1").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("In", wait=True)
-        elif n==5:
+        elif n == 5:
             epics.PV("BL62:E1608_1:Bd1").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("In", wait=True)
-        elif n==6:
+        elif n == 6:
             epics.PV("BL62:E1608_1:Bd1").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("In", wait=True)
-        elif n==7:
+        elif n == 7:
             epics.PV("BL62:E1608_1:Bd1").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("In", wait=True)
-        elif n==8:
+        elif n == 8:
             epics.PV("BL62:E1608_1:Bd1").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("Out", wait=True)
-        elif n==9:
+        elif n == 9:
             epics.PV("BL62:E1608_1:Bd1").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("Out", wait=True)
-        elif n==10:
+        elif n == 10:
             epics.PV("BL62:E1608_1:Bd1").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("Out", wait=True)
-        elif n==11:
+        elif n == 11:
             epics.PV("BL62:E1608_1:Bd1").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("Out", wait=True)
-        elif n==12:
+        elif n == 12:
             epics.PV("BL62:E1608_1:Bd1").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("Out", wait=True)
-        elif n==13:
+        elif n == 13:
             epics.PV("BL62:E1608_1:Bd1").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd4").put("Out", wait=True)
-        elif n==14:
+        elif n == 14:
             epics.PV("BL62:E1608_1:Bd1").put("In", wait=True)
             epics.PV("BL62:E1608_1:Bd2").put("Out", wait=True)
             epics.PV("BL62:E1608_1:Bd3").put("Out", wait=True)
@@ -90,4 +90,4 @@ def filter_number(n):
     else:
         print("You typed a wrong filter number")
 
-filter_number(10)
+filter_number(sys.argv)
